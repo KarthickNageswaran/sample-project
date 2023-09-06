dfb8429e09b075fda544e84a91421eb0652a7ec2
Karthik Nageswaran <karthick.inform@gmail.com>
2023-09-06 15:13:32 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -2,7 +2,7 @@ class sample {
 	public static void main(String[] args) {
 		System.out.println("This is java program updated today");
 		System.out.println("second line printing");
-		System.out.println("Third line added");
+		System.out.println("Third1 1 line added");
 		System.out.println("fourth line printing");
 		System.out.println("fifth line printing");
 		System.out.println("sixth line printing");
